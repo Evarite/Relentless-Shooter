fba9ea3e04e1a5aefab794029dd66bee2fef15fc
Alaksiej <forestingel@gmail.com>
2026-07-02 01:00:52 +0200

    Slot animations created.

---
+++ Assets/Scripts/Inventory/UI/InventorySlotUIScaling.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Relentless.Inventory
+{
+    [RequireComponent(typeof(Animator))]
+    public class InventorySlotUIScaling : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Animator _animator;
+
+        private void Awake() => _animator = GetComponent<Animator>();
+
+        public void OnPointerEnter(PointerEventData eventData) => _animator.SetBool("IsScalingUp", true);
+
+        public void OnPointerExit(PointerEventData eventData) => _animator.SetBool("IsScalingUp", false);
+    }
+}
