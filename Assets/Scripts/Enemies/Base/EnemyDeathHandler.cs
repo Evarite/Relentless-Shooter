bc711e5981f370d175d520ebaf6b3e4afc8e8c0e
Alaksiej <forestingel@gmail.com>
2026-07-12 10:51:01 +0200

    EnemyDeathHandler now returns an enemy to the pool instead of destroying it.

--- Assets/Scripts/Enemies/Base/EnemyDeathHandler.cs
+++ Assets/Scripts/Enemies/Base/EnemyDeathHandler.cs
@@ -1,13 +1,19 @@
 ﻿using Relentless.HealthSystem;
+using Relentless.Pooling;
 using UnityEngine;
 
 namespace Relentless.Enemies.Base
 {
+    [RequireComponent(typeof(PooledEntity))]
     public class EnemyDeathHandler : MonoBehaviour, IDeathHandler
     {
+        private PooledEntity _pe;
+
+        private void Awake() => _pe = GetComponent<PooledEntity>();
+
         public void Die()
         {
-            Destroy(gameObject);
+            _pe.Return();
         }
     }
 }
