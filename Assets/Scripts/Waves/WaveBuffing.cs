2d710635ddccee1715e912feecc334642dd494c8
Alaksiej <forestingel@gmail.com>
2026-08-04 11:01:37 +0200

    WaveBuffing implemented.

--- Assets/Scripts/Waves/WaveBuffing.cs
+++ Assets/Scripts/Waves/WaveBuffing.cs
@@ -1,13 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Relentless.Waves
 {
     [RequireComponent(typeof(WaveTimer))]
     public class WaveBuffing : MonoBehaviour
     {
+        [Header("Buff Iterations")]
+        [Min(1)]
+        [SerializeField] private int _buffIterations = 1;
+
+        [SerializeField] private WaveEnemiesBuffData _buffData;
+
+        private List<BuffOption> _buffs = new List<BuffOption>();
+
         private WaveTimer _timer;
 
-        private void Awake() => _timer = GetComponent<WaveTimer>();
+        private void Awake()
+        {
+            _timer = GetComponent<WaveTimer>();
+
+            _buffs.Add(new BuffOption
+                (
+                _buffData.HealthBuffWeight,
+                () => { BuffsModifiers.HealthBuffModifier += _buffData.BuffStep; }
+                ));
+
+            _buffs.Add(new BuffOption
+                (
+                _buffData.DamageBuffWeight,
+                () => { BuffsModifiers.DamageBuffModifier += _buffData.BuffStep; }
+                ));
+
+            _buffs.Add(new BuffOption
+                (
+                _buffData.SpeedBuffWeight,
+                () => { BuffsModifiers.SpeedBuffModifier += _buffData.BuffStep; }
+                ));
+
+            _buffs.Add(new BuffOption
+                (
+                _buffData.IterationBuffWeight,
+                () => { _buffIterations += _buffData.IterationStep; }
+                ));
+
+            _buffs.Add(new BuffOption
+                (
+                _buffData.EmptyWeight,
+                () => { }
+                ));
+        }
 
         private void OnEnable() => _timer.OnTimeRanOut += Buff;
 
@@ -15,7 +57,36 @@ namespace Relentless.Waves
 
         private void Buff()
         {
-            Debug.Log("The event worked");
+            for (int i = 0; i <= _buffIterations; i++)
+            {
+                float weight = Random.Range(0f, _buffData.TotalWeight);
+                float currentWeight = 0f;
+
+                foreach (var buff in _buffs)
+                {
+                    currentWeight += buff.Weight;
+                    if (weight <= currentWeight)
+                    {
+                        buff.Buff?.Invoke();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private class BuffOption
+        {
+            private float _weight;
+            private System.Action _buff;
+
+            public BuffOption(float Weight, System.Action Buff)
+            {
+                _weight = Weight;
+                _buff = Buff;
+            }
+
+            public float Weight { get => _weight; }
+            public System.Action Buff { get => _buff; }
         }
     }
 }
--- Assets/Scripts/Waves/WaveEnemiesBuffData.cs
+++ Assets/Scripts/Waves/WaveEnemiesBuffData.cs
@@ -1,17 +1,32 @@
-﻿using Relentless.Utilities.WeightedRandom;
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Relentless.Waves
 {
     [CreateAssetMenu(fileName = "Wave Buffs Data", menuName = "Relentless/Waves/Buffs Data")]
     public class WaveEnemiesBuffData : ScriptableObject
     {
-        [Header("Buff Iterations")]
-        [Min(1)]
-        [SerializeField] private int _buffIterations = 1;
+        [Header("Buffs Weight")]
+        [SerializeField] private float _speedBuffWeight = 2f;
+        [SerializeField] private float _healthBuffWeight = 3f;
+        [SerializeField] private float _damageBuffWeight = 5f;
+        [SerializeField] private float _iterationBuffWeight = 0.5f;
+        [SerializeField] private float _emptyWeight = 1f;
+        private float _totalWeight = 0f;
 
-        [Header("Buffs")]
-        [SerializeField] private WeightedRandomList<Action> _buffs;
+        [Header("Buffs Step")]
+        [SerializeField] private float _buffStep = 0.05f;
+        [SerializeField] private int _iterationStep = 1;
+
+        private void Awake() =>
+            _totalWeight = _speedBuffWeight + _healthBuffWeight + _damageBuffWeight + _emptyWeight;
+
+        public float SpeedBuffWeight { get => _speedBuffWeight; }
+        public float HealthBuffWeight { get => _healthBuffWeight; }
+        public float DamageBuffWeight { get => _damageBuffWeight; }
+        public float EmptyWeight { get => _emptyWeight; }
+        public float BuffStep { get => _buffStep; }
+        public float TotalWeight { get => _totalWeight; }
+        public float IterationBuffWeight { get => _iterationBuffWeight; }
+        public int IterationStep { get => _iterationStep; }
     }
 }
